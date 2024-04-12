33773e554a699c3e530ca1fe3a9291d900564023
som3canadian <somecanadian0@gmail.com>
2024-04-12 16:19:40 -0400

    formatting

--- workers/cf-redirector-worker/src/index.js
+++ workers/cf-redirector-worker/src/index.js
@@ -1,44 +1,42 @@
-import { verify } from '@tsndr/cloudflare-worker-jwt'
+import {verify} from '@tsndr/cloudflare-worker-jwt'
 
 addEventListener('fetch', event => {
-	event.respondWith(handleRedirectorRequest(event));
+  event.respondWith(handleRedirectorRequest(event));
 });
 
 async function handleRedirectorRequest(event) {
-	const req = event.request;
-	const PRESHARED_ID_HEADER = req.headers.get(ID_HEADER);
-	if (!PRESHARED_ID_HEADER) {
-		return unauthorizedResponse();
-	}
-	const LISTEN_ENDPOINT_VAR = setDestUrl(PRESHARED_ID_HEADER);
-	const workerURL = new URL(req.url);
+  const req = event.request;
+  const PRESHARED_ID_HEADER = req.headers.get(ID_HEADER);
+  if (! PRESHARED_ID_HEADER) {
+    return unauthorizedResponse();
+  }
+  const LISTEN_ENDPOINT_VAR = setDestUrl(PRESHARED_ID_HEADER);
+  const workerURL = new URL(req.url);
   const WORKER_ENDPOINT = workerURL.origin + "/";
   // console.log(WORKER_ENDPOINT);
-	const newPath = req.url.replace(WORKER_ENDPOINT, "");
+  const newPath = req.url.replace(WORKER_ENDPOINT, "");
   const newDestUrl = LISTEN_ENDPOINT_VAR + newPath;
-	// console.log(newDestUrl);
-	// Extract token from the Authorization header
-	const bearer_token = req.headers.get('Authorization')?.replace('Bearer ', '');
-	if (!bearer_token) {
-		return unauthorizedResponse();
-	}
-	const isValid = await verify(bearer_token, JWT_SECRET);
-	if (isValid) {
-		// console.log(bearer_token);
-		// send the request to the listener
-		const listener_response = await listenerRequest(req, newDestUrl);
-		return listener_response;
-	}
-	else {
-		return unauthorizedResponse();
-	}
+  // console.log(newDestUrl);
+  // Extract token from the Authorization header
+  const bearer_token = req.headers.get('Authorization') ?. replace('Bearer ', '');
+  if (! bearer_token) {
+    return unauthorizedResponse();
+  }
+  const isValid = await verify(bearer_token, JWT_SECRET);
+  if (isValid) {
+    // console.log(bearer_token);
+    // send the request to the listener
+    const listener_response = await listenerRequest(req, newDestUrl);
+    return listener_response;
+  } else {
+    return unauthorizedResponse();
+  }
 }
 
 async function listenerRequest(req, newDestUrl) {
   const newModifiedHeaders = new Headers(req.headers);
   newModifiedHeaders.set("CF-Access-Client-Id", SERVICE_CF_ID);
   newModifiedHeaders.set("CF-Access-Client-Secret", SERVICE_CF_SECRET);
-
   const thisModifiedRequest = new Request(newDestUrl, {
     body: req.body,
     headers: newModifiedHeaders,
@@ -49,8 +47,12 @@ async function listenerRequest(req, newDestUrl) {
 }
 
 function unauthorizedResponse() {
-  return new Response(JSON.stringify({ "Error": "Unauthorized" }, null, 2), {
+  return new Response(JSON.stringify({
+    "Error": "Unauthorized"
+  }, null, 2), {
     status: 401,
-    headers: { "content-type": "application/json;charset=UTF-8" }
+    headers: {
+      "content-type": "application/json;charset=UTF-8"
+    }
   });
 }
